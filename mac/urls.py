ec7b08ccddbb5a67dd5cdec0386fade2581e0912
khizer-khan-ejaz <khizerkhanejaz7860@gmail.comgit config --global user.name khizer-khan-ejazgit config --global user.name khizer-khan-ejazgit config --global user.email khizerkhanejaz7860@gmail.com>
2022-03-04 21:32:18 +0530

    Initial commit

--- mac/urls.py
+++ mac/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include,url
-
-
+from django.conf import settings
+from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('manar.urls')), 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),   
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),   
     
 ]
