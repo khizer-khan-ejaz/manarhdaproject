e1aa2c4caee99d79be149a3a5919812d57ad42ad
khizer-khan-ejaz <khizerkhanejaz7860@gmail.comgit config --global user.name khizer-khan-ejazgit config --global user.name khizer-khan-ejazgit config --global user.email khizerkhanejaz7860@gmail.com>
2022-03-04 22:18:01 +0530

    Initial commit

--- mac/settings.py
+++ mac/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@7ww6oxvk@r6y+*ev$d4qns-57b8bjqsst*0)%9x226fs!$0e*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://manarhuda.herokuapp.com/',]
 
